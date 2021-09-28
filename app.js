dd6db516e8177ec793b8d3e2e9cd8e79cee7a457
asdfg-langsd <84178005+asdfg-langsd@users.noreply.github.com>
2021-09-28 11:51:03 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,5 @@
-
-class Resource {
-  constructor(cost = "None", costResource = "None", boost = "None", produce = "None", initial = 0, buyAmount = 1, cd = 1, prodAmount = 1){
-    this.cost = cost
-    this.boost = boost
-    this.produce = produce
-    this.amount = initial
-    this.buyAmount = buyAmount
-    this.costResource = costResource
-    this.cd = cd
-    this.prodAmount = prodAmount
-  }
-  buy(){
-    this.amount += this.buyAmount
-    this.costResource -= this.cost
-  }
-};
-
-
-let seed = new Resource(1, tree, produce = tree, initial = 1)
-let tree = new Resource()
+function increment(){
+  ppcBoost = basePpcBoost
+  ppc = basePpc * (ppcBoost / 100)
+  point += ppc
+}
